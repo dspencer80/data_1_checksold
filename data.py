5986b44e659fa036387a94cadb55abd313aec9ec
David Spencer <91159580+dspencer80@users.noreply.github.com>
2023-02-19 22:46:48 -0500

    add_dictionary

--- data.py
+++ data.py
@@ -1,4 +1,10 @@
 print("hello world!")
 
 dog_names = ["bonnie", "troubles", "sparky"]
-print(dog_names[0])
+print(dog_names[2])
+
+
+cars = {"Jeep": "Grand Cherokee",
+        "GMC": "Sierra"}
+
+print(cars.get("Jeep"))
